699e7f22e6926bc2fb57ab2d96d5204bdf9f81e7
Kristian Lopez <kristian.lom@gmail.com>
2020-05-14 09:44:40 -0500

    First commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const element = <h1>Hello, World!</h1>;
+
+const container = document.getElementById("app");
+
+// ReactDOM.render(__qué__, __dónde__);
+ReactDOM.render(element, container);
